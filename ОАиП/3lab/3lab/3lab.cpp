b36331b5aa6af0191ec17a67b05ae0541e31629d
Marta <118897672+martineidger@users.noreply.github.com>
2023-03-22 12:52:16 +0300

    Add files via upload

---
+++ ОАиП/3lab/3lab/3lab.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h> 
+
+int main()
+{
+	char c = getchar();
+	printf("    %c%c%c%c \n", c, c, c, c);
+	printf("   %c%c%c%c%c%c\n", c, c, c, c, c, c);
+	printf("  %c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c);
+	printf(" %c%c%c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c, c, c);
+	printf("  %c%c%c%c%c%c%c%c\n", c, c, c, c, c, c, c, c);
+	printf("   %c%c%c%c%c%c\n", c, c, c, c, c, c);
+	printf("    %c%c%c%c\n", c, c, c, c);
+	return 0;
+}
